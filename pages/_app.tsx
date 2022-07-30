8f7511b00791bb3cadc718e34b8773a76dba0450
zknoller <hi@zknoller.com>
2022-07-30 17:10:05 -0400

    Test less than barebones metamask connector

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Web3ReactProvider } from '@web3-react/core'
+import Web3 from 'web3';
+
+function getLibrary(provider: any) {
+  return new Web3(provider)
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const supportedChainIds = [1]
+  const connectors = {
+    injected: {},
+    
+  }
+
+
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+        <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
+}
+
+export default MyApp
